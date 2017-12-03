e03bedcc411d6be2df973159123c55bc2f5cca4d
David Gutierrez <dcgutierrez93@yahoo.com>
2017-12-03 12:01:31 -0600

    Simple Car Loan Calculator Working

--- CarLoan.java
+++ CarLoan.java
@@ -1,27 +1,26 @@
 public class CarLoan {
 
-    // Int Var carLoan.
-    int carLoan = 1000;
-    // Int loanLength.
-    int loanLength = 3;
-    // Int interestRate.
-    int interestRate = 5;
-    // Int downPayment.
-    int downPayment = 2000;
-
-    if (loanLength <= 0 || interestRate <= 0) {
-        System.out.println("Error! You must take out a valid a valid car loan.");
-    } else if (downPayment >= carLoan) {
-        System.out.println("The car can be paid in full.");
-    } else {
-        int remainingBalance = carLoan - downPayment;
-        int months = loanLength * 12;
-        int monthlyBalance = remainingBalance / months;
-        int interest = monthlyBalance * interestRate / 100;
-        int monthlyPayment = monthlyBalance + interest;
-        System.out.println(monthlyPayment);
-    }
     public static void main(String[] args) {
+        // Int Var carLoan.
+        int carLoan = 10000;
+        // Int loanLength.
+        int loanLength = 3;
+        // Int interestRate.
+        int interestRate = 5;
+        // Int downPayment.
+        int downPayment = 2000;
 
+        if (loanLength <= 0 || interestRate <= 0) {
+            System.out.println("Error! You must take out a valid a valid car loan.");
+        } else if (downPayment >= carLoan) {
+            System.out.println("The car can be paid in full.");
+        } else {
+            int remainingBalance = carLoan - downPayment;
+            int months = loanLength * 12;
+            int monthlyBalance = remainingBalance / months;
+            int interest = monthlyBalance * interestRate / 100;
+            int monthlyPayment = monthlyBalance + interest;
+            System.out.println(monthlyPayment);
+        }
     }
 }
