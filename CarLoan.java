b9707f71b88bb76419040eda36c5821bccdb8857
dcgutierrez93 <dcgutierrez93@yahoo.com>
2017-12-03 11:37:14 -0600

    added files and variables

---
+++ CarLoan.java
@@ -0,0 +1,11 @@
+public class CarLoan {
+
+    // Create int var carLoan = 1000
+    int carLoan = 1000;
+    int loanLength = 3;
+    int interestRate = 5;
+    int downPayment = 2000;
+    public static void main(String[] args) {
+
+    }
+}
